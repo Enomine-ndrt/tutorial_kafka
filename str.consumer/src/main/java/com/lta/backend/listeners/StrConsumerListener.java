75e560c8823199bfd03f3f5b6d5eb633622e6615
Enomine-ndrt <luis.w0587@gmail.com>
2026-07-13 22:08:41 -0600

    add:(config)strconsumerlisterner partition

--- str.consumer/src/main/java/com/lta/backend/listeners/StrConsumerListener.java
+++ str.consumer/src/main/java/com/lta/backend/listeners/StrConsumerListener.java
@@ -2,35 +2,30 @@ package com.lta.backend.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"0"})
+            , containerFactory = "strContainerFactory")
     public void listener1(String message){
-        log.info("Recibiendo un mensaje {} ", message);
         log.info("LISTENER ::: Recibiendo un mensaje {} ", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"1"})
+            , containerFactory = "strContainerFactory")
     public void listener2(String message){
-        log.info("Recibiendo un mensaje {} ", message);
         log.info("LISTENER2 ::: Recibiendo un mensaje {} ", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
     public void listener3(String message){
-        log.info("Recibiendo un mensaje {} ", message);
         log.info("LISTENER3 ::: Recibiendo un mensaje {} ", message);
     }
-
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listene4(String message){
-        log.info("Recibiendo un mensaje {} ", message);
-        log.info("LISTENER4 ::: Recibiendo un mensaje {} ", message);
-    }
-
 
 }
